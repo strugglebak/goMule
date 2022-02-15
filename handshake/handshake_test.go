9fdad6d6a7b1879a08fd40de2b2b90d4ab3a4bd1
strugglebak <strugglebak@gmail.com>
2022-02-15 15:35:28 +0800

    feat: add handshake test

---
+++ handshake/handshake_test.go
@@ -0,0 +1,123 @@
+package handshake
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSerialize(t *testing.T) {
+	tests := map[string]struct {
+		input  *Handshake
+		output []byte
+	} {
+		"serialize message": {
+			input: &Handshake{
+				ProtocolIdentifier:	"BitTorrent protocol",
+				InfoHash: 					[20]byte{
+					134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116,
+				},
+				PeerID:   					[20]byte{
+					1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+				},
+			},
+			output: []byte{
+				19, 66, 105, 116, 84, 111, 114, 114, 101, 110, 116, 32, 112, 114, 111, 116, 111, 99, 111, 108, 0, 0, 0, 0, 0, 0, 0, 0, 134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+			},
+		},
+		"different protocol identifier": {
+			input: &Handshake{
+				ProtocolIdentifier:	"BitTorrent protocol, but cooler?",
+				InfoHash: 					[20]byte{
+					134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116,
+				},
+				PeerID:   					[20]byte{
+					1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+				},
+			},
+			output: []byte{
+				32, 66, 105, 116, 84, 111, 114, 114, 101, 110, 116, 32, 112, 114, 111, 116, 111, 99, 111, 108, 44, 32, 98, 117, 116, 32, 99, 111, 111, 108, 101, 114, 63, 0, 0, 0, 0, 0, 0, 0, 0, 134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		buffer := test.input.serialize()
+		assert.Equal(t, test.output, buffer)
+	}
+}
+
+func TestBuildHandshake(t *testing.T) {
+	infoHash := [20]byte{
+		134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116,
+	}
+	peerID := [20]byte{
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+	}
+	handshake := buildHandshake(infoHash, peerID)
+	expected := &Handshake{
+		ProtocolIdentifier: "BitTorrent protocol",
+		InfoHash: 					[20]byte{
+			134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116,
+		},
+		PeerID:   					[20]byte{
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+		},
+	}
+	assert.Equal(t, expected, handshake)
+}
+
+func TestRead(t *testing.T) {
+	tests := map[string]struct {
+		input  []byte
+		output *Handshake
+		fails  bool
+	}{
+		"parse handshake into struct": {
+			input: []byte{
+				19, 66, 105, 116, 84, 111, 114, 114, 101, 110, 116, 32, 112, 114, 111, 116, 111, 99, 111, 108, 0, 0, 0, 0, 0, 0, 0, 0, 134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+			},
+			output: &Handshake{
+				ProtocolIdentifier: "BitTorrent protocol",
+				InfoHash: 					[20]byte{
+					134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116,
+				},
+				PeerID:   					[20]byte{
+					1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+				},
+			},
+			fails: false,
+		},
+		"empty": {
+			input:  []byte{},
+			output: nil,
+			fails:  true,
+		},
+		"not enough bytes": {
+			input:  []byte{
+				19, 66, 105, 116, 84, 111, 114, 114, 101, 110, 116, 32, 112, 114, 111, 116, 111, 99, 111,
+			},
+			output: nil,
+			fails:  true,
+		},
+		"protocol identifier length is 0": {
+			input:  []byte{
+				0, 0, 0,
+			},
+			output: nil,
+			fails:  true,
+		},
+	}
+
+	for _, test := range tests {
+		reader := bytes.NewReader(test.input)
+		handshake, err := read(reader)
+		if test.fails {
+			assert.NotNil(t, err)
+		} else {
+			assert.Nil(t, err)
+		}
+		assert.Equal(t, test.output, handshake)
+	}
+}
