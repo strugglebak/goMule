3bc2b5074d9f3d5bac11e151fb1ea2cff7194baa
strugglebak <strugglebak@gmail.com>
2022-02-17 17:14:03 +0800

    fix: fixup bencodeInfo struct field tags

--- torrent_file/torrent_file.go
+++ torrent_file/torrent_file.go
@@ -81,7 +81,7 @@ func (t *TorrentFile) DownloadAndSaveTorrentFile(savePath string, port uint16) e
 
 type bencodeInfo struct {
 	Pieces				string	`bencode:"pieces"`
-	PieceLength		int			`bencode:"pieces length"`
+	PieceLength		int			`bencode:"piece length"`
 	Length				int			`bencode:"length"`
 	Name					string	`bencode:"name"`
 }
