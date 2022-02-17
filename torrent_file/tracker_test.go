3e75b3c362340331df2a8f3687d5e5f1e1e527b5
strugglebak <strugglebak@gmail.com>
2022-02-17 16:27:35 +0800

    chore: change tracker test

--- torrent_file/tracker_test.go
+++ torrent_file/tracker_test.go
@@ -11,28 +11,27 @@ import (
 )
 
 func TestBuildTrackerURL(t *testing.T) {
-	torrentFile := TorrentFile {
+	to := TorrentFile{
 		Announce: "http://bttracker.debian.org:6969/announce",
-		InfoHash: [20]byte{ 183, 161, 12, 130, 76, 207, 59, 26, 19, 131, 12, 89, 56, 129, 45, 111, 83, 71, 10, 68 },
+		InfoHash: [20]byte{216, 247, 57, 206, 195, 40, 149, 108, 204, 91, 191, 31, 134, 217, 253, 207, 219, 168, 206, 182},
 		PieceHashes: [][20]byte{
-			{ 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106 },
-			{ 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48 },
+			{49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106},
+			{97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48},
 		},
 		PieceLength: 262144,
 		Length:      351272960,
 		Name:        "debian-10.2.0-amd64-netinst.iso",
 	}
-
 	peerID := [20]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
-	const Port uint16 = 6882
-	url, err := torrentFile.BuildTrackerURL(peerID, Port)
-	expected := "http://bttracker.debian.org:6969/announce?compact=1&downloaded=0&info_hash=%B7%A1%0C%82L%CF%3B%1A%13%83%0CY8%81-oSG%0AD&left=351272960&peer_id=%01%02%03%04%05%06%07%08%09%0A%0B%0C%0D%0E%0F%10%11%12%13%14&port=6882&uploaded=0"
+	const port uint16 = 6881
+	url, err := to.BuildTrackerURL(peerID, port)
+	expected := "http://bttracker.debian.org:6969/announce?compact=1&downloaded=0&info_hash=%D8%F79%CE%C3%28%95l%CC%5B%BF%1F%86%D9%FD%CF%DB%A8%CE%B6&left=351272960&peer_id=%01%02%03%04%05%06%07%08%09%0A%0B%0C%0D%0E%0F%10%11%12%13%14&port=6881&uploaded=0"
 	assert.Nil(t, err)
 	assert.Equal(t, url, expected)
 }
 
 func TestRequestPeers(t *testing.T) {
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		response := []byte(
 			"d" +
 				"8:interval" + "i900e" +
@@ -43,27 +42,25 @@ func TestRequestPeers(t *testing.T) {
 				}) + "e")
 		w.Write(response)
 	}))
-
-	defer server.Close()
-
-	torrentFile := TorrentFile {
-		Announce: server.URL,
-		InfoHash: [20]byte{ 183, 161, 12, 130, 76, 207, 59, 26, 19, 131, 12, 89, 56, 129, 45, 111, 83, 71, 10, 68 },
+	defer ts.Close()
+	tf := TorrentFile{
+		Announce: ts.URL,
+		InfoHash: [20]byte{216, 247, 57, 206, 195, 40, 149, 108, 204, 91, 191, 31, 134, 217, 253, 207, 219, 168, 206, 182},
 		PieceHashes: [][20]byte{
-			{ 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106 },
-			{ 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48 },
+			{49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106},
+			{97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48},
 		},
 		PieceLength: 262144,
 		Length:      351272960,
 		Name:        "debian-10.2.0-amd64-netinst.iso",
 	}
-	peerID := [20]byte{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 }
-	const Port uint16 = 6882
+	peerID := [20]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	const port uint16 = 6882
 	expected := []peers.Peer{
-		{IP: net.IP{ 192, 0, 2, 123 }, Port: 6881 },
-		{IP: net.IP{ 127, 0, 0, 1 }, Port: 6889 },
+		{IP: net.IP{192, 0, 2, 123}, Port: 6881},
+		{IP: net.IP{127, 0, 0, 1}, Port: 6889},
 	}
-	p, err := torrentFile.RequestPeers(peerID, Port)
+	p, err := tf.RequestPeers(peerID, port)
 	assert.Nil(t, err)
 	assert.Equal(t, expected, p)
 }
